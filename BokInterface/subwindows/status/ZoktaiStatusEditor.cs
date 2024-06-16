17122cb96d75ee50ee5357ff830e85390a8fd0ea
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2024-06-16 12:46:59 +0200

    Fixed typo

--- BokInterface/subwindows/status/ZoktaiStatusEditor.cs
+++ BokInterface/subwindows/status/ZoktaiStatusEditor.cs
@@ -86,7 +86,7 @@ namespace BokInterface.Status {
                                 case "str":
                                 case "agi":
                                     /**
-                                     * For stats wz also update the "persistent" stat address
+                                     * For stats we also update the "persistent" stat address
                                      * 
                                      * We do this because updating "current" stat value is not enough,
                                      * when switching room the game would set back the old values
