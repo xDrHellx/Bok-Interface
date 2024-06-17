eb788bc12982b54e9eebe4eb786d3a5cee402470
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2024-06-18 00:29:15 +0200

    Added key items editor
    - Added KeyItemsEditor basis class & subclasses for each game.
    - Added properties & methods for initializing the current game's key items editor (when available).
    - Added the button to open it for Bok 2.
    
    TODO :
    - Adjust the winform elements positions & sizes for the key items editor.
    - Fix an issue preventing slot values from being retrieved & set properly (need to find IWRAM memory addresses to combine with each slot's address).

--- BokInterface/interfaces/ZoktaiInterface.cs
+++ BokInterface/interfaces/ZoktaiInterface.cs
@@ -30,6 +30,7 @@ namespace BokInterface {
             _bok2_djangoGunSkill = new();
         private Button _bok2_editStatusBtn = new(),
             _bok2_editInventoryBtn = new(),
+            _bok2_editKeyItemsBtn = new(),
             _bok2_editEquipsBtn = new(),
             _bok2_editWeaponsBtn = new(),
             _bok2_editMagicsBtn = new();
@@ -187,7 +188,8 @@ namespace BokInterface {
 
             _bok2_editStatusBtn = WinFormHelpers.CreateButton("editStatuts", "Status", 6, 19, 75, 23, editGroupBox);
             _bok2_editInventoryBtn = WinFormHelpers.CreateButton("editItems", "Items", 6, 46, 75, 23, editGroupBox);
-            _bok2_editEquipsBtn = WinFormHelpers.CreateButton("editEquips", "Equips", 6, 73, 75, 23, editGroupBox);
+            _bok2_editKeyItemsBtn = WinFormHelpers.CreateButton("editKeyItems", "Key items", 6, 73, 75, 23, editGroupBox);
+            // _bok2_editEquipsBtn = WinFormHelpers.CreateButton("editEquips", "Equips", 6, 73, 75, 23, editGroupBox);
             _bok2_editWeaponsBtn = WinFormHelpers.CreateButton("editWeapons", "Weapons", 6, 100, 75, 23, editGroupBox);
             _bok2_editMagicsBtn = WinFormHelpers.CreateButton("editMagics", "Magics", 6, 127, 75, 23, editGroupBox);
 
@@ -199,6 +201,7 @@ namespace BokInterface {
             // Add onclick events
             _bok2_editStatusBtn.Click += new System.EventHandler(OpenStatusEditor);
             _bok2_editInventoryBtn.Click += new System.EventHandler(OpenInventoryEditor);
+            _bok2_editKeyItemsBtn.Click += new System.EventHandler(OpenKeyItemsEditor);
             // _bok2_editEquipsBtn.Click += new System.EventHandler(OpenEquipsEditor);
             // _bok2_editWeaponsBtn.Click += new System.EventHandler(OpenWeaponsEditor);
             // _bok2_editMagicsBtn.Click += new System.EventHandler(OpenMagicsEditor);
