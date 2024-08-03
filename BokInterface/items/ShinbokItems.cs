05a644beed444b33b755d22eab96ca749cbccf8f
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2024-08-03 14:40:31 +0200

    Fixed wrong item values & names
    - Fixed wrong item values.
    - Changed some names to be more accurate compared to english versions of previous games.

--- BokInterface/items/ShinbokItems.cs
+++ BokInterface/items/ShinbokItems.cs
@@ -36,45 +36,45 @@ namespace BokInterface.Items {
             Items.Add("Bearnut", new ShinbokItem("Bearnut", 18, "bearnut"));
             Items.Add("Speed Nut", new ShinbokItem("Speed Nut", 19, "speed_nut"));
             Items.Add("Banana", new ShinbokItem("Banana", 20, "banana"));
-            Items.Add("Chocolate Banana", new ShinbokItem("Chocolate Banana", 20, "chocolate_banana"));
-            Items.Add("Tiptoe Nut", new ShinbokItem("Tiptoe Nut", 21, "tiptoe_nut"));
-            Items.Add("Redshroom", new ShinbokItem("Redshroom", 22, "redshroom"));
-            Items.Add("Blueshroom", new ShinbokItem("Blueshroom", 23, "blueshroom"));
-            Items.Add("See-All Nut", new ShinbokItem("See-All Nut", 24, "see_all_nut"));
-            Items.Add("Warp Leaf", new ShinbokItem("Warp Leaf", 25, "warp_leaf"));
-            Items.Add("Mr. Rainnot", new ShinbokItem("Mr. Rainnot", 26, "mr_rainnot"));
-            Items.Add("tonniaR .rM", new ShinbokItem("tonniaR .rM", 27, "tonniar_rm"));
-            Items.Add("Sunny Clog", new ShinbokItem("Sunny Clog", 28, "sunny_clog"));
-            Items.Add("Loser Stick", new ShinbokItem("Loser Stick", 29, "loser_stick"));
-            Items.Add("Winner Stick", new ShinbokItem("Winner Stick", 30, "winner_stick"));
-            Items.Add("The High Priestess", new ShinbokItem("The High Priestess", 31, "the_high_priestess"));
-            Items.Add("The Empress", new ShinbokItem("The Empress", 32, "the_empress"));
-            Items.Add("Strength", new ShinbokItem("Strength", 33, "strength"));
-            Items.Add("Wheel of Fortune", new ShinbokItem("Wheel of Fortune", 34, "wheel_of_fortune"));
-            Items.Add("The Moon", new ShinbokItem("The Moon", 35, "the_moon"));
-            Items.Add("The Sun", new ShinbokItem("The Sun", 36, "the_sun"));
-            Items.Add("Judgement", new ShinbokItem("Judgement", 37, "judgement"));
+            Items.Add("Chocolate Banana", new ShinbokItem("Chocolate Banana", 21, "chocolate_banana"));
+            Items.Add("Tiptoe Nut", new ShinbokItem("Tiptoe Nut", 22, "tiptoe_nut"));
+            Items.Add("Redshroom", new ShinbokItem("Redshroom", 23, "redshroom"));
+            Items.Add("Blueshroom", new ShinbokItem("Blueshroom", 24, "blueshroom"));
+            Items.Add("See-All Nut", new ShinbokItem("See-All Nut", 25, "see_all_nut"));
+            Items.Add("Warp Leaf", new ShinbokItem("Warp Leaf", 26, "warp_leaf"));
+            Items.Add("Mr. Rainnot", new ShinbokItem("Mr. Rainnot", 27, "mr_rainnot"));
+            Items.Add("tonniaR .rM", new ShinbokItem("tonniaR .rM", 28, "tonniar_rm"));
+            Items.Add("Sunny Clog", new ShinbokItem("Sunny Clog", 29, "sunny_clog"));
+            Items.Add("Loser Stick", new ShinbokItem("Loser Stick", 30, "loser_stick"));
+            Items.Add("Winner Stick", new ShinbokItem("Winner Stick", 31, "winner_stick"));
+            Items.Add("The High Priestess", new ShinbokItem("The High Priestess", 32, "the_high_priestess"));
+            Items.Add("The Empress", new ShinbokItem("The Empress", 33, "the_empress"));
+            Items.Add("Strength", new ShinbokItem("Strength", 34, "strength"));
+            Items.Add("Wheel of Fortune", new ShinbokItem("Wheel of Fortune", 35, "wheel_of_fortune"));
+            Items.Add("The Moon", new ShinbokItem("The Moon", 36, "the_moon"));
+            Items.Add("The Sun", new ShinbokItem("The Sun", 37, "the_sun"));
+            Items.Add("Judgement", new ShinbokItem("Judgement", 38, "judgement"));
         }
 
         ///<summary>Init key items instances</summary>
         private void InitKeyItems() {
             // KeyItems.Add("Empty slot", new ShinbokItem("Empty Slot", 65535));
-            KeyItems.Add("Dark Card", new ShinbokItem("Dark Card", 38, "dark_card"));
-            // KeyItems.Add("Master Otenko", new ShinbokItem("Master Otenko", 39)); // Unused
-            KeyItems.Add("Sword License", new ShinbokItem("Sword License", 40, "sword_license"));
-            KeyItems.Add("PET", new ShinbokItem("PET", 41, "pet"));
-            KeyItems.Add("Vector Coffin", new ShinbokItem("Vector Coffin", 42, "vector_coffin"));
-            KeyItems.Add("Red Key", new ShinbokItem("Red Key", 43, "red_key"));
-            KeyItems.Add("Yellow Key", new ShinbokItem("Yellow Key", 44, "yellow_key"));
-            KeyItems.Add("Green Key", new ShinbokItem("Green Key", 45, "green_key"));
-            KeyItems.Add("Blue Key", new ShinbokItem("Blue Key", 46)); // Unused
-            KeyItems.Add("Pile Parts", new ShinbokItem("Pile Parts", 47, "pile_parts"));
-            // KeyItems.Add("Dungeon Item 2", new ShinbokItem("Dungeon Item 2", 48)); // Unused
-            KeyItems.Add("Fate Goddess Statue", new ShinbokItem("Fate Goddess Statue", 49, "fate_goddess_statue"));
-            KeyItems.Add("Existence Goddess Statue", new ShinbokItem("Existence Goddess Statue", 50, "existence_goddess_statue"));
-            KeyItems.Add("Necessity Goddess Statue", new ShinbokItem("Necessity Goddess Statue", 51, "necessity_goddess_statue"));
-            // KeyItems.Add("Mission Item 4", new ShinbokItem("Mission Item 4", 52)); // Unused
-            // KeyItems.Add("Mission Item 5", new ShinbokItem("Mission Item 5", 53)); // Unused
+            KeyItems.Add("Dark Card", new ShinbokItem("Dark Card", 39, "dark_card"));
+            // KeyItems.Add("Master Otenko", new ShinbokItem("Master Otenko", 40)); // Unused
+            KeyItems.Add("Sword License", new ShinbokItem("Sword License", 41, "sword_license"));
+            KeyItems.Add("PET", new ShinbokItem("PET", 42, "pet"));
+            KeyItems.Add("Vector Coffin", new ShinbokItem("Vector Coffin", 43, "vector_coffin"));
+            KeyItems.Add("Circle Key", new ShinbokItem("Circle Key", 44, "circle_key"));
+            KeyItems.Add("Triangle Key", new ShinbokItem("Triangle Key", 45, "triangle_key"));
+            KeyItems.Add("Square Key", new ShinbokItem("Square Key", 46, "square_key"));
+            KeyItems.Add("Cross Key", new ShinbokItem("Cross Key", 47, "cross_key"));
+            KeyItems.Add("Pile Parts", new ShinbokItem("Pile Parts", 48, "pile_parts"));
+            // KeyItems.Add("Dungeon Item 2", new ShinbokItem("Dungeon Item 2", 49)); // Unused
+            KeyItems.Add("Fate Goddess Statue", new ShinbokItem("Fate Goddess Statue", 50, "fate_goddess_statue"));
+            KeyItems.Add("Existence Goddess Statue", new ShinbokItem("Existence Goddess Statue", 51, "existence_goddess_statue"));
+            KeyItems.Add("Necessity Goddess Statue", new ShinbokItem("Necessity Goddess Statue", 52, "necessity_goddess_statue"));
+            // KeyItems.Add("Mission Item 4", new ShinbokItem("Mission Item 4", 53)); // Unused
+            // KeyItems.Add("Mission Item 5", new ShinbokItem("Mission Item 5", 54)); // Unused
         }
     }
 }
