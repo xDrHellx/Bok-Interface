12238ca37459f62c6f8e47974f5887c9f2a8fe92
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2024-09-07 20:32:22 +0200

    Refactoring regarding Magic classes
    - Turned the Magic class into an abstract class, similar to what we do for Weapons or Items.
    - Added the ZoktaiMagic class for Bok 2 magics instances.
    - Updated the dictionnaries in the ZoktaiMagics class.

--- BokInterface/magics/Magic.cs
+++ BokInterface/magics/Magic.cs
@@ -1,15 +1,16 @@
 using System.Drawing;
 
 namespace BokInterface.Magics {
-    ///<summary>Class representing a magic</summary>
-    class Magic {
+    ///<summary>Base class for representing a magic</summary>
+    abstract class Magic {
 
-        public readonly string name;
+        public string name;
         /// <summary>Magic type (Luna, Sol, Dark, ...)</summary>
         public string type;
+        public string iconName;
         public Image? icon = null;
         /// <summary>Description (usually corresponds to the in-game text</summary>
-        public readonly string description;
+        public string description;
 
         public Magic(string name, string type, string icon = "", string description = "") {
             this.name = name;
@@ -17,6 +18,7 @@ namespace BokInterface.Magics {
             this.description = description;
 
             // If an icon was specified try getting & setting it to the property
+            iconName = icon;
             if (icon != "") {
                 try {
                     this.icon = (Image)Properties.Resources.ResourceManager.GetObject(icon);
--- BokInterface/magics/ZoktaiMagics.cs
+++ BokInterface/magics/ZoktaiMagics.cs
@@ -21,41 +21,40 @@ namespace BokInterface.Magics {
 
         ///<summary>Init magic instances for Luna magics</summary>
         private void InitLunaMagics() {
-            Luna.Add("Enchant Sol", new Magic("Enchant Sol", "Luna", "enchant_sol"));
-            Luna.Add("Enchant Dark", new Magic("Enchant Dark", "Luna", "enchant_dark"));
-            Luna.Add("Enchant Flame", new Magic("Enchant Flame", "Luna", "enchant_flame"));
-            Luna.Add("Enchant Frost", new Magic("Enchant Frost", "Luna", "enchant_frost"));
-            Luna.Add("Enchant Cloud", new Magic("Enchant Cloud", "Luna", "enchant_cloud"));
-            Luna.Add("Enchant Earth", new Magic("Enchant Earth", "Luna", "enchant_earth"));
-            Luna.Add("Transform", new Magic("Transform", "Luna", "transform"));
-            Luna.Add("Rising Sun", new Magic("Rising Sun", "Luna", "rising_sun"));
+            Luna.Add("Enchant Sol", new ZoktaiMagic("Enchant Sol", "Luna", "enchant_sol"));
+            Luna.Add("Enchant Dark", new ZoktaiMagic("Enchant Dark", "Luna", "enchant_dark"));
+            Luna.Add("Enchant Flame", new ZoktaiMagic("Enchant Flame", "Luna", "enchant_flame"));
+            Luna.Add("Enchant Frost", new ZoktaiMagic("Enchant Frost", "Luna", "enchant_frost"));
+            Luna.Add("Enchant Cloud", new ZoktaiMagic("Enchant Cloud", "Luna", "enchant_cloud"));
+            Luna.Add("Enchant Earth", new ZoktaiMagic("Enchant Earth", "Luna", "enchant_earth"));
+            Luna.Add("Transform", new ZoktaiMagic("Transform", "Luna", "transform"));
+            Luna.Add("Rising Sun", new ZoktaiMagic("Rising Sun", "Luna", "rising_sun"));
         }
 
         ///<summary>Init magic instances for Sol magics</summary>
         private void InitSolMagics() {
-            Sol.Add("Freeze", new Magic("Freeze", "Sol", "freeze"));
-            Sol.Add("Dash", new Magic("Dash", "Sol", "dash"));
-            Sol.Add("Healing", new Magic("Healing", "Sol", "healing"));
-            Sol.Add("Dynamite", new Magic("Dynamite", "Sol", "dynamite"));
+            Sol.Add("Freeze", new ZoktaiMagic("Freeze", "Sol", "freeze"));
+            Sol.Add("Dash", new ZoktaiMagic("Dash", "Sol", "dash"));
+            Sol.Add("Healing", new ZoktaiMagic("Healing", "Sol", "healing"));
+            Sol.Add("Dynamite", new ZoktaiMagic("Dynamite", "Sol", "dynamite"));
         }
 
         ///<summary>Init magic instances for Dark magics</summary>
         private void InitDarkMagics() {
-            Dark.Add("Sleeping", new Magic("Sleeping", "Dark", "sleeping"));
-            Dark.Add("Change Bat", new Magic("Change Bat", "Dark", "change_bat"));
-            Dark.Add("Change Mouse", new Magic("Change Mouse", "Dark", "change_mouse"));
-            Dark.Add("Change Wolf", new Magic("Change Wolf", "Dark", "change_wolf"));
+            Dark.Add("Sleeping", new ZoktaiMagic("Sleeping", "Dark", "sleeping"));
+            Dark.Add("Change Bat", new ZoktaiMagic("Change Bat", "Dark", "change_bat"));
+            Dark.Add("Change Mouse", new ZoktaiMagic("Change Mouse", "Dark", "change_mouse"));
+            Dark.Add("Change Wolf", new ZoktaiMagic("Change Wolf", "Dark", "change_wolf"));
         }
 
         ///<summary>Init magic instances for Sabata</summary>
         private void InitSabataMagics() {
-            Sabata.Add("Zero Shift", new Magic("Zero Shift", "Dark", "zero_shift"));
-            Sabata.Add("Dark Sun", new Magic("Dark Sun", "Dark", "dark_sun"));
+            Sabata.Add("Zero Shift", new ZoktaiMagic("Zero Shift", "Sabata", "zero_shift"));
+            Sabata.Add("Dark Sun", new ZoktaiMagic("Dark Sun", "Sabata", "dark_sun"));
         }
 
         ///<summary>Init the full list containing all magics (mostly used for editors)</summary>
         private void InitFullList() {
-            All.Add("Empty slot", new Magic("Empty slot", ""));
             All = All
                 .Concat(Luna)
                 .Concat(Sol)
