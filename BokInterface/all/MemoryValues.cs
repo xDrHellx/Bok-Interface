b5e5958d4bb89083f434a0457986d3f5e6cdcb14
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2024-10-29 20:54:57 +0100

    Added Bok 3 accessory slots mem addresses & values instances
    - Added memory addresses for accessory inventory slots.
    - Added MemoryValues dictionnary entries for these too.

--- BokInterface/all/MemoryValues.cs
+++ BokInterface/all/MemoryValues.cs
@@ -150,10 +150,11 @@ namespace BokInterface.All {
 
                 int slotNumber = i + 1;
 
-                // Items, key items
+                // Items, durability, key items & accessory slots
                 Inventory.Add("slot" + slotNumber + "_item", new DynamicMemoryValue("slot" + slotNumber + "_item", _shinbokAddresses.Misc["stat"].Address, _shinbokAddresses.Inventory["item_slot_" + slotNumber].Address));
-                Inventory.Add("slot" + slotNumber + "_key_item", new DynamicMemoryValue("slot" + slotNumber + "_key_item", _shinbokAddresses.Misc["stat"].Address, _shinbokAddresses.Inventory["key_item_slot_" + slotNumber].Address));
                 Inventory.Add("slot" + slotNumber + "_durability", new DynamicMemoryValue("slot" + slotNumber + "_durability", _shinbokAddresses.Misc["stat"].Address, _shinbokAddresses.Inventory["item_slot_durability_" + slotNumber].Address));
+                Inventory.Add("slot" + slotNumber + "_key_item", new DynamicMemoryValue("slot" + slotNumber + "_key_item", _shinbokAddresses.Misc["stat"].Address, _shinbokAddresses.Inventory["key_item_slot_" + slotNumber].Address));
+                Inventory.Add("slot" + slotNumber + "_accessory", new DynamicMemoryValue("slot" + slotNumber + "_accessory", _shinbokAddresses.Misc["stat"].Address, _shinbokAddresses.Inventory["accessory_slot_" + slotNumber].Address));
             }
         }
 
