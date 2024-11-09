37b2c691aefdf30312d18eea1fab619f95e2a30e
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2024-11-09 12:19:54 +0100

    Refactoring regarding Bok 3 accessories
    - Updated the ShinbokAccessory to better represent a Bok 3 accessory.
    - Updated the ShinbokAccessories dictionnaries : sets & levels have been added for each accessory.
    
    Sets indicates if the accessory is part of a set.
    Levels will be important later : these determines the possible results of solar forging.

--- BokInterface/accessories/ShinbokAccessory.cs
+++ BokInterface/accessories/ShinbokAccessory.cs
@@ -4,15 +4,21 @@ namespace BokInterface.Accessories {
     ///<summary>Class for representing an accessory in Shinbok</summary>
     class ShinbokAccessory : Accessory {
 
-        public ShinbokAccessory(string name, uint value, string type, string icon = "", int row = 1, int buyPrice = 0, bool crossOver = false, int defense = 0, int weight = 0) : base(name, value, type) {
+        /// <summary>Accessory level (determines results when used for solar forging)</summary>
+        public int level = 1;
+        /// <summary>The name of the set the accessory belongs to (Solar Django, Black Django, Rockman, ...)</summary>
+        public string set = "";
+
+        public ShinbokAccessory(string name, uint value, string type, string icon = "", int row = 1, int buyPrice = 0, bool crossOver = false, int defense = 0, int level = 0, string set = "") : base(name, value, type) {
             this.name = name;
             this.value = value;
             this.type = type;
             this.row = row;
             this.crossOver = crossOver;
             this.defense = defense;
-            this.weight = weight;
             this.buyPrice = buyPrice;
+            this.level = level;
+            this.set = set;
 
             // Price for selling is always the buying price divided by 2 (or 0 if it cannot be sold)
             sellPrice = buyPrice > 0 ? buyPrice / 2 : 0;
