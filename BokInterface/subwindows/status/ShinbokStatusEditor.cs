55a25b925d4c6e14b53e755ebd9fa303edc2807b
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2024-12-29 19:08:18 +0100

    Bok 3 status editor update
    - Adjusted elements sizes & positions.
    - Removed the "possibility" to set values for accessories stats (this wasn't working anyway because the game checks for equipped accessories when switching room).
    - Added a specific treatment for setting stats values.
    
    Simply put : we have to rely on the memory addresses for the "sum of base + cards points" and persistent addresses to update every properly on the fly, as well as making changes stay after switching room.

--- BokInterface/subwindows/status/ShinbokStatusEditor.cs
+++ BokInterface/subwindows/status/ShinbokStatusEditor.cs
@@ -5,6 +5,13 @@ using System.Windows.Forms;
 using BokInterface.Addresses;
 using BokInterface.All;
 
+/**
+ * Note :
+ * 
+ * Due to how the game works, stat points from accessories cannot be updated here
+ * The game checks equipped accessories when switching room
+ */
+
 namespace BokInterface.Status {
     /// <summary>Status editor for Boktai 3</summary>
     class ShinbokStatusEditor : StatusEditor {
@@ -24,7 +31,7 @@ namespace BokInterface.Status {
             Owner = _bokInterface = bokInterface;
             Icon = _bokInterface.Icon;
 
-            SetFormParameters(283, 157);
+            SetFormParameters(241, 157);
 
             // Add the onClose event to the subwindow
             FormClosing += new FormClosingEventHandler(delegate (object sender, FormClosingEventArgs e) {
@@ -43,7 +50,7 @@ namespace BokInterface.Status {
 
             // Sections
             statusGroupBox = WinFormHelpers.CreateCheckGroupBox("editStatusGroup", "Status", 5, 5, 102, 106, control: this);
-            statsGroupBox = WinFormHelpers.CreateCheckGroupBox("editStatsGroup", "Stats", 113, 5, 166, 124, control: this);
+            statsGroupBox = WinFormHelpers.CreateCheckGroupBox("editStatsGroup", "Stats", 113, 5, 124, 124, control: this);
 
             // Status
             WinFormHelpers.CreateLabel("djangoEditHpLabel", "LIFE :", 7, 22, 34, 15, statusGroupBox);
@@ -61,25 +68,19 @@ namespace BokInterface.Status {
 
             WinFormHelpers.CreateLabel("djangoStatsBaseLabel", "Base", 35, 19, 42, 15, control: statsGroupBox, WinFormHelpers.baseStatColor);
             WinFormHelpers.CreateLabel("djangoStatsCardsLabel", "Cards", 77, 19, 42, 15, control: statsGroupBox, WinFormHelpers.cardsStatColor);
-            WinFormHelpers.CreateLabel("djangoStatsEquipsLabel", "Equips", 119, 19, 42, 15, control: statsGroupBox, WinFormHelpers.equipsStatColor);
 
             // Base
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_base_vit", defaultValues["django_base_vit"], 39, 37, 38, 23, maxValue: 100, control: statsGroupBox));
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_base_spr", defaultValues["django_base_spr"], 39, 66, 38, 23, maxValue: 100, control: statsGroupBox));
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_base_str", defaultValues["django_base_str"], 39, 95, 38, 23, maxValue: 100, control: statsGroupBox));
+            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("misc_base_vit", defaultValues["django_base_vit"], 39, 37, 38, 23, maxValue: 100, control: statsGroupBox));
+            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("misc_base_spr", defaultValues["django_base_spr"], 39, 66, 38, 23, maxValue: 100, control: statsGroupBox));
+            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("misc_base_str", defaultValues["django_base_str"], 39, 95, 38, 23, maxValue: 100, control: statsGroupBox));
 
             // Cards
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_cards_vit", defaultValues["django_cards_vit"], 81, 37, 38, 23, maxValue: 100, control: statsGroupBox));
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_cards_spr", defaultValues["django_cards_spr"], 81, 66, 38, 23, maxValue: 100, control: statsGroupBox));
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_cards_str", defaultValues["django_cards_str"], 81, 95, 38, 23, maxValue: 100, control: statsGroupBox));
-
-            // Equips / accessories
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_equips_vit", defaultValues["django_equips_vit"], 123, 37, 38, 23, maxValue: 100, control: statsGroupBox));
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_equips_spr", defaultValues["django_equips_spr"], 123, 66, 38, 23, maxValue: 100, control: statsGroupBox));
-            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("django_equips_str", defaultValues["django_equips_str"], 123, 95, 38, 23, maxValue: 100, control: statsGroupBox));
+            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("misc_cards_vit", defaultValues["django_cards_vit"], 81, 37, 38, 23, maxValue: 100, control: statsGroupBox));
+            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("misc_cards_spr", defaultValues["django_cards_spr"], 81, 66, 38, 23, maxValue: 100, control: statsGroupBox));
+            statusNumericUpDowns.Add(WinFormHelpers.CreateNumericUpDown("misc_cards_str", defaultValues["django_cards_str"], 81, 95, 38, 23, maxValue: 100, control: statsGroupBox));
 
             // Button for setting values & its events
-            Button setValuesButton = WinFormHelpers.CreateButton("setStatusButton", "Set values", 205, 131, 75, 23, this);
+            Button setValuesButton = WinFormHelpers.CreateButton("setStatusButton", "Set values", 163, 131, 75, 23, this);
             setValuesButton.Click += new EventHandler(delegate (object sender, EventArgs e) {
                 // Write the values for 10 frames
                 for (int i = 0; i < 10; i++) {
@@ -98,15 +99,12 @@ namespace BokInterface.Status {
                 defaultValues.Add("django_current_hp", djangoCurrentHp);
                 defaultValues.Add("django_current_ene", _memoryValues.Django["current_ene"].Value);
                 defaultValues.Add("django_current_trc", _memoryValues.Django["current_trc"].Value);
-                defaultValues.Add("django_base_vit", _memoryValues.Django["base_vit"].Value);
-                defaultValues.Add("django_base_spr", _memoryValues.Django["base_spr"].Value);
-                defaultValues.Add("django_base_str", _memoryValues.Django["base_str"].Value);
-                defaultValues.Add("django_cards_vit", _memoryValues.Django["cards_vit"].Value);
-                defaultValues.Add("django_cards_spr", _memoryValues.Django["cards_spr"].Value);
-                defaultValues.Add("django_cards_str", _memoryValues.Django["cards_str"].Value);
-                defaultValues.Add("django_equips_vit", _memoryValues.Django["equips_vit"].Value);
-                defaultValues.Add("django_equips_spr", _memoryValues.Django["equips_spr"].Value);
-                defaultValues.Add("django_equips_str", _memoryValues.Django["equips_str"].Value);
+                defaultValues.Add("django_base_vit", _memoryValues.Misc["base_vit"].Value);
+                defaultValues.Add("django_base_spr", _memoryValues.Misc["base_spr"].Value);
+                defaultValues.Add("django_base_str", _memoryValues.Misc["base_str"].Value);
+                defaultValues.Add("django_cards_vit", _memoryValues.Misc["cards_vit"].Value);
+                defaultValues.Add("django_cards_spr", _memoryValues.Misc["cards_spr"].Value);
+                defaultValues.Add("django_cards_str", _memoryValues.Misc["cards_str"].Value);
             } else {
                 // If HP is unvalid (for example if we are on the title screen or in bike races), use specific values
                 defaultValues.Add("django_current_hp", 100);
@@ -118,9 +116,6 @@ namespace BokInterface.Status {
                 defaultValues.Add("django_cards_vit", 0);
                 defaultValues.Add("django_cards_spr", 0);
                 defaultValues.Add("django_cards_str", 0);
-                defaultValues.Add("django_equips_vit", 0);
-                defaultValues.Add("django_equips_spr", 0);
-                defaultValues.Add("django_equips_str", 0);
             }
 
             return defaultValues;
@@ -151,9 +146,9 @@ namespace BokInterface.Status {
                  * Indicate which sublist to use for setting the value, based on the input field's name
                  * We only split on the first "_"
                  */
-                string[] fieldParts = fields[i].Name.Split(['_'], 2);
+                string[] fieldParts = fields[i].Name.Split(['_'], 3);
                 string subList = fieldParts[0];
-                string memoryValueKey = fieldParts[1];
+                string memoryValueKey = fieldParts[1] + "_" + fieldParts[2];
                 switch (subList) {
                     case "django":
                         if (_memoryValues.Django.ContainsKey(memoryValueKey) == true) {
@@ -164,27 +159,28 @@ namespace BokInterface.Status {
                             _memoryValues.U32[memoryValueKey].Value = (uint)value;
                         }
                         break;
-                    case "solls":
-                        if (_memoryValues.Solls.ContainsKey(memoryValueKey) == true) {
-                            _memoryValues.Solls[memoryValueKey].Value = (uint)value;
-                        } else if (_memoryValues.U16.ContainsKey(memoryValueKey) == true) {
-                            _memoryValues.U16[memoryValueKey].Value = (uint)value;
-                        } else if (_memoryValues.U32.ContainsKey(memoryValueKey) == true) {
-                            _memoryValues.U32[memoryValueKey].Value = (uint)value;
-                        }
-                        break;
-                    case "bike":
-                        if (_memoryValues.Bike.ContainsKey(memoryValueKey) == true) {
-                            _memoryValues.Bike[memoryValueKey].Value = (uint)value;
-                        } else if (_memoryValues.U16.ContainsKey(memoryValueKey) == true) {
-                            _memoryValues.U16[memoryValueKey].Value = (uint)value;
-                        } else if (_memoryValues.U32.ContainsKey(memoryValueKey) == true) {
-                            _memoryValues.U32[memoryValueKey].Value = (uint)value;
-                        }
-                        break;
                     case "misc":
                         if (_memoryValues.Misc.ContainsKey(memoryValueKey) == true) {
-                            _memoryValues.Misc[memoryValueKey].Value = (uint)value;
+                            switch (memoryValueKey) {
+                                case "base_vit":                // Base points from level ups
+                                case "base_spr":
+                                case "base_str":
+                                case "cards_vit":               // Points from cards
+                                case "cards_spr":
+                                case "cards_str":
+                                    /**
+                                     * Due to how the game works we have to set the sum of base + card stats
+                                     * 
+                                     * This allows stats to be updated in the current room
+                                     * and stay when switching room
+                                     */
+                                    _memoryValues.Misc[memoryValueKey].Value = (uint)value;
+                                    UpdateSumBaseCardStat(fieldParts[2], _memoryValues.Misc["base_" + fieldParts[2]].Value, _memoryValues.Misc["cards_" + fieldParts[2]].Value);
+                                    break;
+                                default:                        // Default treatment
+                                    _memoryValues.Misc[memoryValueKey].Value = (uint)value;
+                                    break;
+                            }
                         } else if (_memoryValues.U16.ContainsKey(memoryValueKey) == true) {
                             _memoryValues.U16[memoryValueKey].Value = (uint)value;
                         } else if (_memoryValues.U32.ContainsKey(memoryValueKey) == true) {
@@ -207,6 +203,15 @@ namespace BokInterface.Status {
              */
             if (_bokInterface._previousIsPauseSetting == true) {
                 APIs.Client.Unpause();
+            }
+        }
+
+        /// <summary>Updates the sum of base + card points for a stat</summary>
+        /// <param name="stat">Stat to update (str, spr, str)</param>
+        private void UpdateSumBaseCardStat(string stat, uint basePoints, uint cardPoints) {
+            string key = "sum_base_cards_" + stat;
+            if (_memoryValues.Django.ContainsKey(key) == true) {
+                _memoryValues.Django[key].Value = basePoints + cardPoints;
             }
         }
     }
