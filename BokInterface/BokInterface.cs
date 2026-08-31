4b539d0e846b09868cc1da1f3af523f9d11eebfb
xDrHellx <17726521+xDrHellx@users.noreply.github.com>
2026-08-31 16:17:17 +0200

    Updated version (0.2.4 => 0.2.5)
    - Fixed a bunch of issue with Bok 3 Weapons editor.

--- BokInterface/BokInterface.cs
+++ BokInterface/BokInterface.cs
@@ -27,7 +27,7 @@ public partial class BokInterface : ToolFormBase, IExternalToolForm {
 
     #region Tool properties
 
-    protected override string WindowTitleStatic => "Bok Interface v0.2.4";
+    protected override string WindowTitleStatic => "Bok Interface v0.2.5";
     public override bool BlocksInputWhenFocused => false;
     protected Icon? icon;
     public uint currentGameId;
